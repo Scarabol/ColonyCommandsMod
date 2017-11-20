6e331f5cfd922f4ad5583ae10687878822d58fa5
Scarabol <scarabol@gmail.com>
2017-11-20 09:28:10 +0100

    increase version to 0.7.4

--- src/Commands.cs
+++ src/Commands.cs
@@ -19,7 +19,7 @@ namespace ScarabolMods
     [ModLoader.ModCallback (ModLoader.EModCallbackType.AfterStartup, "scarabol.commands.registercallbacks")]
     public static void AfterStartup ()
     {
-      Pipliz.Log.Write ("Loaded Commands Mod 0.7.3 by Scarabol");
+      Pipliz.Log.Write ("Loaded Commands Mod 0.7.4 by Scarabol");
     }
   }
 
