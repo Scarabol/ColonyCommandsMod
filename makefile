63e8bbfdcd425118dc39db72d06e2c92be1fb04d
Scarabol <scarabol@gmail.com>
2017-11-28 16:21:33 +0100

    incease version to 0.7.7

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 # important variables
 modname = Commands
-version = 0.7.6
+version = 0.7.7
 
 moddir = Scarabol/$(modname)
 zipname = Colony$(modname)Mod-$(version)-mods.zip
--- src/Commands.cs
+++ src/Commands.cs
@@ -19,7 +19,7 @@ namespace ScarabolMods
     [ModLoader.ModCallback (ModLoader.EModCallbackType.AfterStartup, "scarabol.commands.registercallbacks")]
     public static void AfterStartup ()
     {
-      Pipliz.Log.Write ("Loaded Commands Mod 0.7.6 by Scarabol");
+      Pipliz.Log.Write ("Loaded Commands Mod 0.7.7 by Scarabol");
     }
   }
 
