1231dc2b1f83acdded182478c702d48890be0523
adrenalynn <adrenalynn@gmx.com>
2018-04-25 16:43:24 +0200

    distance calculation for BannerNameCommand

--- src/BannerNameCommand.cs
+++ src/BannerNameCommand.cs
@@ -19,7 +19,26 @@ namespace ScarabolMods
 
     public bool TryDoCommand (Players.Player causedBy, string chattext)
     {
-      var closestBanner = BannerTracker.GetClosest (causedBy.VoxelPosition, 5000);
+      var position = causedBy.Position;
+      var closestBanner = null;
+      var shortestDistance = -1;
+      for (var c = 0; c < BannerTracker.GetCount(); c++) {
+        Banner banner;
+        if (BannerTracker.TryGetAtIndex(c, out banner)) {
+          if (banner.Owner == causedBy) {
+            continue;
+          }
+          Vector3Int bannerLocation = banner.KeyLocation;
+          var distX = position.x - bannerLocation.x;
+          var distZ = position.z - bannerLocation.z;
+          var distance = (int)System.Math.Sqrt(System.Math.Pow(distX, 2) + System.Math.Pow(distZ, 2));
+          if (shortestDistance == -1 || distance < shortestDistance) {
+            shortestDistance = distance;
+            closestBanner = banner;
+          }
+        }
+      }
+
       if (closestBanner != null) {
         var ownerName = closestBanner.Owner.Name;
         if (ownerName != null) {
